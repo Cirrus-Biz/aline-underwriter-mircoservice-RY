f2238a7fa2a350511e1b1a0450e75f293f88e9d3
noodleween <happyguy122x@gmail.com>
2022-04-18 13:23:40 -0600

    CB-48 Refactor: application vars shell script

---
+++ application_env_vars.sh
@@ -0,0 +1,14 @@
+export MICROSERVICE=underwriter
+export APP_PORT=8071
+export DB_USERNAME=root
+export DB_PASSWORD=root
+export DB_HOST=127.0.0.1
+export DB_PORT=3306
+export DB_NAME=alinedb
+export ENCRYPT_SECRET_KEY='thisisagreatsecretkey!!!'
+export JWT_SECRET_KEY='WmZq4t7w!z%C&F)J@NcRfUjXn2r5u8x/'
+echo "Exports have been applied"
+echo "Updating ${MICROSERVICE} microservice"
+mvn clean install -U
+echo "Running ${MICROSERVICE} microservice"
+mvn spring-boot:run -pl ${MICROSERVICE}-microservice/
